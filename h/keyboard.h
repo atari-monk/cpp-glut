c0a7a0f62bfc5e22e9cba0d87c39965a6cf50406
atari-monk <atari.monk1@gmail.com>
2023-02-08 19:57:14 +0100

    add projs

--- h/keyboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-class Keyboard {
-public:
-	void key(unsigned char key, int x, int y);
-};
-
-#endif
--- h/resizer.h
+++
@@ -1,9 +0,0 @@
-#ifndef RESIZER_H
-#define RESIZER_H
-
-class Resizer {
-public:
-	void resize(int width, int height);
-};
-
-#endif
